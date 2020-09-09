da2a3e9dca6327e08dd7ce8114f5b693151c9fc0
Morad17 <morad123@hotmail.co.uk>
2020-09-09 13:14:11 +0000

    finished nav

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,23 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+burger.addEventListener('click', () =>{
+    //Toggle the Name
+    nav.classList.toggle('nav-active');
+  
+    //Animations of Links
+    navLinks.forEach((link, i)=>{
+        if(link.style.animation){
+            link.style.animation = '';
+        } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.7}s`;
+        }
+        });  
+        //Burger Animation
+        burger.classList.toggle('toggle');
 
-
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('nav-active');
     });
 }
 

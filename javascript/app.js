78505a79b1507d10b7dcabadb31d5c157a57748b
Morad17 <morad123@hotmail.co.uk>
2020-10-05 19:01:26 +0000

    css responsive updates

--- javascript/app.js
+++ javascript/app.js
@@ -1,29 +1,29 @@
                                     
                     //Nav Javascripy//
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
     
-burger.addEventListener('click', () =>{
-    //Toggle the Name
-    nav.classList.toggle('nav-active');
+// burger.addEventListener('click', () =>{
+//     //Toggle the Name
+//     nav.classList.toggle('nav-active');
   
-    //Animations of Links
-    navLinks.forEach((link, i)=>{
-        if(link.style.animation){
-            link.style.animation = '';
-        } else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.7}s`;
-        }
-        });  
-        //Burger Animation
-        burger.classList.toggle('toggle');
+//     //Animations of Links
+//     navLinks.forEach((link, i)=>{
+//         if(link.style.animation){
+//             link.style.animation = '';
+//         } else{
+//             link.style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.7}s`;
+//         }
+//         });  
+//         //Burger Animation
+//         burger.classList.toggle('toggle');
 
-    });
-}
-navSlide();
+//     });
+// }
+// navSlide();
                 //Progress Bar Slider//
     // const focusBar = document.getElementsByClassName('focus-bars')[0];
     // setInterval(()=>{
